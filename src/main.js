25a275e78974c519a205fc0e982d7e6fd8c70b60
johannestj <johannestj@student.uia.no>
2025-03-04 14:54:38 +0100

    added routing from one point to another

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import L, { map } from 'leaflet';
 import './style.css';
 import { initializeMap } from './map.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initializeMap();
+});
+
+map.on('moveend', function (event) {
+  console.log(map.getCenter(), map.getZoom());
 });
